b9d2c5b499649c4b446ad195d936c7d88d8b4061
DmitryZzz <yes.zimin@gmail.com>
2019-10-03 09:49:49 +0300

    5.8. JavaDoc. Code convention (#2)

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -7,22 +7,42 @@ package ru.job4j.calculator;
 
 public class Calculator {
 
+    /**
+     * Сложение чисел: first + second
+     * @param first - число
+     * @param second - число
+     */
     public static void add(double first, double second) {
         double result = first + second;
         System.out.println(first + " + " + second + " = " + result);
     }
 
+    /**
+     * Деление чисел: first / second
+     * @param first - число
+     * @param second - число
+     */
     public static void div(double first, double second) {
         double result = 0;
         if (second != 0) result = (first / second);
         System.out.println(first + " / " + second + " = " + result);
     }
 
+    /**
+     * Произведение чисел: first * second
+     * @param first - число
+     * @param second - число
+     */
     public static void multiply(double first, double second) {
         double result = first * second;
         System.out.println(first + " * " + second + " = " + result);
     }
 
+    /**
+     * Разность чисел: first - second
+     * @param first - число
+     * @param second - число
+     */
     public static void subtrack(double first, double second) {
         double result = first - second;
         System.out.println(first + " - " + second + " = " + result);
