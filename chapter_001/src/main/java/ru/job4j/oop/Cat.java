ad21df452ab7312a5890646a40963c4591e19869
Dmitry Zimin <yes.zimin@gmail.com>
2019-10-07 21:51:12 +0300

    1.1. Создание объекта

---
+++ chapter_001/src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,16 @@
+package ru.job4j.oop;
+
+/**
+ * Class Cat.
+ *
+ * @author Dmitry Zimin
+ * @version 1
+ * @since 2019-10-07
+ */
+public class Cat {
+
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+        Cat sparky = new Cat();
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,16 @@
+package ru.job4j.oop;
+
+/**
+ * Class Dog.
+ *
+ * @author Dmitry Zimin
+ * @version 1
+ * @since 2019-10-07
+ */
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
