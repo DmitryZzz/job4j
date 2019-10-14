1f46e4058bbf8cd70307872c8e309ba24b3f38d6
Dmitry Zimin <yes.zimin@gmail.com>
2019-10-14 22:34:02 +0300

    4.2. Статические методы

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,8 +8,7 @@ public class StartUI {
         boolean run = true;
         while (run) {
             this.showMenu();
-            System.out.print("Select: ");
-            int select = Integer.valueOf(input.askStr(""));
+            int select = input.askInt("Select: ");
             if (select == 0) {
                 StartUI.createItem(input, tracker);
             } else if (select == 1) {
